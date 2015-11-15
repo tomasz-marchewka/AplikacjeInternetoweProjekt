1acafdb5c6b0ce4ada92c015f8dce2c2568c0b9f
mkostka <127131@stud.prz.edu.pl>
2015-11-15 21:09:48 +0100

    Utworzenie bazowego modelu.

---
+++ src/main/java/pl/prz/aip/model/BaseModel.java
@@ -0,0 +1,38 @@
+package pl.prz.aip.model;
+
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
+import javax.persistence.Version;
+
+@MappedSuperclass
+public class BaseModel<ID> {
+	
+	public static final String ID = "id";
+	
+	private ID id;
+	
+	private int version;
+
+	@Transient
+	public ID getId() {
+		return id;
+	}
+
+	public void setId(ID id) {
+		this.id = id;
+	}
+
+	@Version
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+	
+	
+	
+	
+
+}
