c56b73d9dc67b2f7f1864de09d583d67bd35c735
Tomasz Marchewka <tomaszmarchewka@hotmail.com>
2015-11-16 20:05:19 +0100

    - list all pracownik by controller.

--- src/main/java/pl/prz/aip/web/DzialController.java
+++ src/main/java/pl/prz/aip/web/DzialController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import pl.prz.aip.dao.DzialRepository;
 import pl.prz.aip.model.Dzial;
+import pl.prz.aip.repository.DzialRepository;
 
 @RestController
 public class DzialController {
