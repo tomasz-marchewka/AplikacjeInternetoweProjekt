a45687d95d028766ac75a9484d6fb8d00879a2f0
Tomasz Marchewka <tomaszmarchewka@hotmail.com>
2015-11-16 18:52:00 +0100

    - connection to database, - new derby dependencies.

--- src/main/java/pl/prz/aip/Application.java
+++ src/main/java/pl/prz/aip/Application.java
@@ -1,17 +1,32 @@
 package pl.prz.aip;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class Application {
+public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
+	@Override
+	public void run(String... arg0) throws Exception {
+		System.out.println("Test");
+		DataSource ds = jdbcTemplate.getDataSource();
+		System.out.println(ds);
+		
+	}
 }
