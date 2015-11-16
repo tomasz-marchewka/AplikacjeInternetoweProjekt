d06dbc35721620c9eea89c6946f03e45edd1587c
Tomasz Marchewka <tomaszmarchewka@hotmail.com>
2015-11-16 19:27:41 +0100

    - simple database operation.

--- src/main/java/pl/prz/aip/Application.java
+++ src/main/java/pl/prz/aip/Application.java
@@ -1,32 +1,36 @@
 package pl.prz.aip;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
+
+import pl.prz.aip.dao.DzialRepository;
+import pl.prz.aip.model.Dzial;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class Application implements CommandLineRunner {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-    
-    @Autowired
-    JdbcTemplate jdbcTemplate;
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Autowired
+	DzialRepository dzialRepo;
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		System.out.println("Test");
-		DataSource ds = jdbcTemplate.getDataSource();
-		System.out.println(ds);
-		
+		Dzial d1 = new Dzial();
+		d1.setNazwa("Robienie dzia³");
+		d1.setOpis("Dzia³ robienia dzia³");
+		dzialRepo.save(d1);
+		for (Dzial dzial : dzialRepo.findAll()) {
+			System.out.println(dzial.getOpis());
+		}
+
 	}
 }
--- src/main/java/pl/prz/aip/model/Dzial.java
+++ src/main/java/pl/prz/aip/model/Dzial.java
@@ -7,28 +7,29 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "DZIAL")
 public class Dzial extends BaseModel<Integer> {
-	
+
 	public static final String OPIS = "opis";
 	public static final String NAZWA = "nazwa";
-	
+
 	private String opis;
 	private String nazwa;
-	
+
 	@Column(name = "OPIS")
 	public String getOpis() {
 		return opis;
 	}
+
 	public void setOpis(String opis) {
 		this.opis = opis;
 	}
+
 	@Column(name = "NAZWA")
 	public String getNazwa() {
 		return nazwa;
 	}
+
 	public void setNazwa(String nazwa) {
 		this.nazwa = nazwa;
 	}
-	
-	
 
 }
