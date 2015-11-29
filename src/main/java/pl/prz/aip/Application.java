9b1cdeda8082ddb98a630b7ac3ece89fc180f1ea
Tomasz Marchewka <tomaszmarchewka@hotmail.com>
2015-11-29 23:16:15 +0100

    - reorganizacja projektu.

--- src/main/java/pl/prz/aip/Application.java
+++ src/main/java/pl/prz/aip/Application.java
@@ -2,12 +2,8 @@ package pl.prz.aip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableAutoConfiguration
@@ -16,18 +12,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	// Cross-origin resource sharing
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/pracownicy").allowedOrigins(
-						"http://localhost:9000");
-			}
-		};
 	}
 
 }
